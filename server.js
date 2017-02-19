ba544ce716659abe958489b5b5c9a33ec8b23e80
Himesh <himesh.rupareliya@wwindia.com>
2017-02-19 09:58:07 +0400

    Updated DB connection details

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/DemoApi');
+dbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/DemoApi'
+mongoose.connect(dbUrl);
 
 var router = require('./route');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.json({message: 'Welcome to demo api'});
